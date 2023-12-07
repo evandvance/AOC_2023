d82b59dacffbe9fd4533dce22c3c76a6132dd504
Evan Vance <72946268+evandvance@users.noreply.github.com>
2023-12-06 22:46:43 -0600

    finished 04

--- Days/04.py
+++ Days/04.py
@@ -2,22 +2,49 @@ import re
 inputs = './inputs/04.txt'
 test = './test/04test.txt'
 
-with open(test) as file:
+with open(inputs) as file:
     data = file.readlines()
 
 def part1(data):
-    return_list = []
+    return_score = 0
 
     for i,line in enumerate(data):
+        # print(line)
+        score = 0
         line = re.sub(f'Card {i+1}:','',line)
-        line= line.split('|')
+        line = line.split('|')
         for char in line[0].split():
-            if char in line[1]:
-                return_list.append(int(char))
+            temp = line[1].split()
+            if char in temp:
+                if score == 0:
+                    score += 1
+                else:
+                    score *= 2
+        return_score += score
 
-    return return_list
+    return return_score
 
 def part2(data):
-    pass
+    cards = []
 
-print(sum(part1(data)))
+    for i,line in enumerate(data):
+        # print(line)
+        score = 0
+        line = re.sub(f'Card {i+1}:','',line)
+        line = line.split('|')
+        for char in line[0].split():
+            temp = line[1].split()
+            if char in temp:
+                score += 1
+        cards.append(score) 
+
+    temp = [1 for x in cards]
+    for i, score in enumerate(cards):
+            for j in range(score):
+                temp[i+j+1] += temp[i]
+
+    return sum(temp)
+
+# print(part1(data))
+
+print(part2(data))
