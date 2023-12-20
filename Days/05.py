3429dcf704da443df2741c3feea84b70c1e84949
Evan Vance <72946268+evandvance@users.noreply.github.com>
2023-12-19 23:26:52 -0600

    Day 5 pt1 and brute pt2 done

--- Days/05.py
+++ Days/05.py
@@ -1,5 +1,54 @@
 inputs = './inputs/05.txt'
 test = './test/05test.txt'
 
-with open(test) as file:
-    data = file.readlines()
+with open(inputs) as file:
+    data = {item[0]: item[1].split('\n') for item in [item.split(':') for item in file.read().split('\n\n')]}
+
+def get_next_value(seed,map_list):
+    for ele in map_list:
+        ele = ele.split()
+        if ele == []:
+            continue
+        if int(seed) in range(int(ele[1]), int(ele[1]) + int(ele[-1])):
+            return int(ele[0]) + (int(seed) - int(ele[1]))
+    return seed
+
+def map_seed(seed, data):
+    r_dict = {'seed':seed}
+
+    for key, val in data.items():
+        if key == 'seeds':
+            continue
+        r_dict[key.replace(' map','').split('-')[-1]] = get_next_value(r_dict[key.split('-')[0]],val)
+    
+    return r_dict
+
+def part_one(data):
+    solved_map = [map_seed(seed,data) for seed in data['seeds'][0].split()]
+    smallest = 999999999999999999999999999999999999
+    for seed in solved_map:
+        if seed['location'] < smallest:
+            smallest = seed['location']
+
+    return smallest
+
+def part_two(data):
+    seeds = data['seeds'][0].split()
+    seed_range = [seed for seed in range(int(seeds[0]),int(seeds[0]) + int(seeds[1]))]
+
+    for seed in range(int(seeds[2]), int(seeds[2]) + int(seeds[3])):
+        if seed not in seed_range:
+            seed_range.append(seed)
+
+    solved_map = [map_seed(seed,data) for seed in seed_range]
+
+    smallest = 999999999999999999999999999999999999
+    for seed in solved_map:
+        if seed['location'] < smallest:
+            smallest = seed['location']
+
+    return smallest
+
+if __name__ == "__main__":
+    # print(f"Part one: {part_one(data)}")
+    print(f"Part two: {part_two(data)}")
