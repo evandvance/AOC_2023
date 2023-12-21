3c86f4e8faab826f3c6f13b2db8e2efd6e01a2f5
Evan Vance <72946268+evandvance@users.noreply.github.com>
2023-12-21 13:23:09 -0600

    added some division logiic

--- Days/05.py
+++ Days/05.py
@@ -1,6 +1,8 @@
 inputs = './inputs/05.txt'
 test = './test/05test.txt'
 
+with open(test) as file:
+    data = {item[0]: item[1].split('\n') for item in [item.split(':') for item in file.read().split('\n\n')]}
 
 def memorize(func):
     cache = {}
@@ -15,9 +17,6 @@ def memorize(func):
 
     return wrapper
 
-with open(inputs) as file:
-    data = {item[0]: item[1].split('\n') for item in [item.split(':') for item in file.read().split('\n\n')]}
-
 @memorize
 def get_next_value(seed,map_list):
     for ele in map_list:
@@ -28,7 +27,6 @@ def get_next_value(seed,map_list):
             return int(ele[0]) + (int(seed) - int(ele[1]))
     return seed
 
-@memorize
 def map_seed(seed, data):
     r_dict = {'seed':seed}
 
@@ -40,6 +38,13 @@ def map_seed(seed, data):
     print(f'Seed location: {r_dict['location']}')
     return r_dict
 
+def check_smallest(solved_map, smallest):
+    for seed in solved_map:
+        if seed['location'] < smallest:
+            smallest = seed['location']
+
+    return smallest
+
 def part_one(data):
     solved_map = [map_seed(seed,data) for seed in data['seeds'][0].split()]
     smallest = 999999999999999999999999999999999999
@@ -50,20 +55,42 @@ def part_one(data):
     return smallest
 
 def part_two(data):
-    seeds = data['seeds'][0].split()
-    seed_range = [seed for seed in range(int(seeds[0]),int(seeds[0]) + int(seeds[1]))]
-
-    for seed in range(int(seeds[2]), int(seeds[2]) + int(seeds[3])):
-        if seed not in seed_range:
-            seed_range.append(seed)
-
-    print('Seeds Generated')
-    solved_map = [map_seed(seed,data) for seed in seed_range]
-
     smallest = 999999999999999999999999999999999999
-    for seed in solved_map:
-        if seed['location'] < smallest:
-            smallest = seed['location']
+    seeds = data['seeds'][0].split()
+
+    #The strategy I am trying is to break the seeds up into quarters and finding the smallest in each quarter (similar to binary search)
+
+    seed_range_one = {"start":int(seeds[0]), "half":(int(seeds[0]) + int(seeds[1]))//2, "end":int(seeds[0]) + int(seeds[1])}
+    print("Seed_range_One Generated....")
+
+    seed_range = range(seed_range_one['start'], seed_range_one["half"])
+    map_1 = [map_seed(seed,data) for seed in seed_range]
+
+    smallest = check_smallest(map_1,smallest)
+    print(smallest)
+
+    seed_range = range(seed_range_one['half'], seed_range_one["end"])
+    map_2 = [map_seed(seed,data) for seed in seed_range]
+
+    smallest = check_smallest(map_2,smallest)
+
+    print(smallest)
+
+    seed_range_two = {"start":int(seeds[2]), "half":(int(seeds[2]) + int(seeds[3]))//2, "end":int(seeds[2]) + int(seeds[3])}
+
+    print("Seed_range_two Generated....")
+
+    seed_range = range(seed_range_two['start'], seed_range_two["half"])
+    map_1 = [map_seed(seed,data) for seed in seed_range]
+
+    smallest = check_smallest(map_1,smallest)
+    print(smallest)
+
+    seed_range = range(seed_range_two['half'], seed_range_two["end"])
+    map_2 = [map_seed(seed,data) for seed in seed_range]
+
+    smallest = check_smallest(map_2,smallest)
+    print(smallest)
 
     return smallest
 
